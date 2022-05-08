812da4738f408dda8baa6300ff455f4cd13de262
Victoria Lojek <victoria.lojek@web.de>
2022-05-09 01:45:42 +0200

    Create basic server

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const aNumber=5;
-const test = "oops";
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
