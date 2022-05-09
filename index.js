4ad0560bdb47d7a198c482739f4dda4282979611
Victoria Lojek <victoria.lojek@web.de>
2022-05-09 21:21:18 +0200

    Added region support

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-const express = require('express')
+const express = require("express")
 const app = express()
-const port = 3000
+const axios = require("axios").default
+const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get("/testing/:region", (req, res) => {
+  let region = req.params.region
+  const sendGetRequest = async () => {
+    try {
+      const regionInfo = await axios.get("https://restcountries.com/v3.1/region/" + region)
+      region = regionInfo.data[0].region
+
+      const sum = regionInfo.data.reduce((prev, current) => {
+        return prev + current.population
+      }, 0)
+
+      const maxNumber = regionInfo.data.reduce((prev, current) => (prev.population > current.population ? prev : current)).population
+      const maxName = regionInfo.data.reduce((prev, current) => (prev.population > current.population ? prev : current)).name.common
+      const maxEmoji = regionInfo.data.reduce((prev, current) => (prev.population > current.population ? prev : current)).flag
+
+      res.send(`The total sum of population of ${region} is ${sum} people.
+      <br><br> The largest amount lives in ${maxEmoji} ${maxName} at ${maxNumber}. 
+      <br><br> That's a lot of people.`)
+    } catch (err) {
+      res.send("Uh oh.")
+      console.error(err)
+    }
+  }
+
+  sendGetRequest()
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Listening on port ${port}`)
 })
